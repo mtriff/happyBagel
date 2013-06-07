69295202b80538e2910ba735d9653b3f8c1caa46
lawmicha <mlaw135@gmail.com>
2013-06-07 16:03:29 -0400

    redirect to room instances

--- app.js
+++ app.js
@@ -1,29 +1,81 @@
 
-/*
-declaration and instantiating nodejs modules
-	declaring express, and instantiating
-	creating http server
-	instantiating socket.io
-*/
+
 var express = require('express'), 
 	app = express(),
 	server = require('http').createServer(app),
-	io = require('socket.io').listen(server);
+	io = require('socket.io').listen(server),
+	fs = require('fs'),
+	mime = require('mime'),
+	path = require('path');
+
+io.set('log level', 1);
 
 var note = require("./models/note.js");
-/*
-tell our express app to use our public directory to serve files
-*/
+
 app.configure(function() {
-    app.use(express.static(__dirname + '/public'));
+	app.use(express.favicon());//serve default favicon
+	app.use(express.logger('dev'));//output development-friendly colored logs
+	app.use(express.bodyParser());//parse request bodies
+	app.use(express.methodOverride());
+	
+	app.use(express.static(__dirname + '/public'));
+	app.use(app.router);
 });
 
 
-/* Wiring up socket.io, event listeners and callbacks
+function send404(response) {
+	response.writeHead(404, {'Content-Type': 'text/plain'});
+	response.write('Error 404: resource not found.');
+	response.end();
+}
 
-we're simply broadcasting what event happened so any client listening can be notified
+function sendFile(response, filePath, fileContents){
+	response.writeHead(
+		200,
+		{"content-type":mime.lookup(path.basename(filePath))}
+	);
+	response.end(fileContents);
+}
 
-*/
+function serveStatic(response, absPath){
+	
+	//check if file exists
+	fs.exists(absPath, function (exists){
+		if (exists){
+			//read file from disk
+			fs.readFile(absPath, function (err, data){
+				if(err){
+					send404(response);
+				}
+				else {
+					//and serve file from disk
+					sendFile(response, absPath, data);
+				}
+			})
+		} 
+		else {
+			send404(response);
+		}
+	});
+
+}
+
+
+
+app.get("/*", function (req, res){
+	console.log("Get request for " + req.url);	
+
+	// TODO: change this 
+	var filePath = false;
+	filePath = 'public/main.html';
+	var absPath = './' + filePath;
+	serveStatic(res, absPath);
+	// ---------------
+
+	//TODO: grab room data from db for that room
+});
+
+//socket stuff
 io.sockets.on('connection', function(socket) {
 	//load all notes
 	note.findAll(socket);
